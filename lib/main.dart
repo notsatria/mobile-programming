3c569c6532eb91a6481779d57bac7a1c18c4ec05
notsatria <buanasatriaa@gmail.com>
2022-10-05 09:41:43 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(primaryColor: Colors.purple),
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+  ));
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(begin: Alignment.topLeft, colors: [
+            Color(0xFF8A2387),
+            Color(0xFFE94057),
+            Color(0xFFE27121),
+          ])),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Image.asset('images/logo.png'),
+              SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 480,
+                width: 325,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'Selamat Datang di Polines',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Silahkan Login Menggunakan Akun Anda',
+                      style: TextStyle(fontSize: 15, color: Colors.grey),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 250,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Alamat Email',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 250,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 40, 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Lupa Password',
+                            style: TextStyle(color: Colors.orangeAccent[700]),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 250,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.lightBlue,
+                        ),
+                        child: Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
